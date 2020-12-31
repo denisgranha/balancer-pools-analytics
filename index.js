0777ea668d3573f088cefa033436024ab96c3f38
Denis Granha <denisgranha@gmail.com>
2020-12-31 18:45:46 +0100

    add gitignore and get pools functionality with basic table

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const axios = require('axios').default;
+const moment = require('moment')
 
 const API_URL = "https://api.thegraph.com/subgraphs/name/balancer-labs/balancer"
 
 const WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 const DAI = "0x6b175474e89094c44da98b954eedeac495271d0f"
 
+/**
+ * Get all Balancer Shared pools of WETH and DAI (exclusively)
+ */
 async function getPools(){
 
   // We need to do two requests and merge them because I haven't found a way to query pools
@@ -22,32 +26,17 @@ async function getPools(){
             where: {
               tokensList: ["${WETH}", "${DAI}"],
               publicSwap: true
+              liquidity_gt: 1000
             }
           ){
             id
-            publicSwap
-            finalized
             swapFee
-            totalWeight
-            totalShares
-            tokensList
+            liquidity
+            swapsCount
+            createTime
             tokens {
-              id
-              address
-              balance
-              decimals
               symbol
               denormWeight
-            }
-            shares {
-              id
-              poolId {
-                id
-              }
-              userAddress {
-                id
-              }
-              balance
             }
           }
         }   
@@ -66,32 +55,17 @@ async function getPools(){
             where: {
               tokensList: ["${DAI}", "${WETH}"],
               publicSwap: true
+              liquidity_gt: 1000
             }
           ){
             id
-            publicSwap
-            finalized
             swapFee
-            totalWeight
-            totalShares
-            tokensList
+            liquidity
+            swapsCount
+            createTime
             tokens {
-              id
-              address
-              balance
-              decimals
               symbol
               denormWeight
-            }
-            shares {
-              id
-              poolId {
-                id
-              }
-              userAddress {
-                id
-              }
-              balance
             }
           }
         }   
@@ -102,10 +76,36 @@ async function getPools(){
   return poolResponsePair1.data.data.pools.concat(poolResponsePair2.data.data.pools)
 }
 
+/**
+ * 
+ */
+
 async function run(){
   const pools = await getPools()
 
-  console.log(`There are ${pools.length} pools on Balancer`)
+  console.log(`There are ${pools.length} pools on Balancer of WETH - DAI with more than 1k USD of liquidity`)
+
+  const formattedPools = pools.map(pool => {
+    pool.swapFee = `${pool.swapFee*100}%`
+    let totalWeight = parseInt(pool.tokens[0].denormWeight) + parseInt(pool.tokens[1].denormWeight)
+    let weightFactor = 100/totalWeight
+    pool.weights = `${pool.tokens[0].denormWeight*weightFactor}% ${pool.tokens[0].symbol} | ${pool.tokens[1].denormWeight*weightFactor}% ${pool.tokens[1].symbol}`
+    pool.liquidity = parseInt(pool.liquidity)
+    pool.createTime = moment(pool.createTime*1000).fromNow()
+    delete pool.tokens
+    return pool
+  })
+
+  formattedPools.sort(function(a, b) {
+    var keyA = a.liquidity,
+      keyB = b.liquidity;
+    // Compare the 2 dates
+    if (keyA > keyB) return -1;
+    if (keyA < keyB) return 1;
+    return 0;
+  });
+
+  console.table(formattedPools)
 }
 
 run()
