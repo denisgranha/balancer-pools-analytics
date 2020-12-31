7ed3947b4d42e4da4866b1856bb14eaa0b59dd1a
Denis Granha <denisgranha@gmail.com>
2020-12-31 16:47:22 +0100

    clarification comment

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 const DAI = "0x6b175474e89094c44da98b954eedeac495271d0f"
 
 async function getPools(){
+
+  // We need to do two requests and merge them because I haven't found a way to query pools
+  // that only contains two specific tokens.
+  // The graph returns a different response based on the token list order. For two tokens that's fine
+  // for more... that grows factorial
   const poolResponsePair1 = await axios({
     url: API_URL,
     method: 'post',
